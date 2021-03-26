d07f8397552c7fe98cddfc5ba6223e945298bca6
lolgamer0126 <ochkofficial@gmail.com>
2021-03-26 18:02:57 +0800

    fix video yt

--- videos/models.py
+++ videos/models.py
@@ -4,8 +4,7 @@ from cloudinary_storage.storage import RawMediaCloudinaryStorage
 
 class Video(models.Model):
     title = models.CharField(max_length=200)
-    video = models.FileField(upload_to='videos/', blank=True, storage=RawMediaCloudinaryStorage(),
-                              validators=[validate_video])    
+    video = models.CharField(max_length=200)  
     picture = models.ImageField(upload_to='photos/')
     def __str__(self):
         return self.title
